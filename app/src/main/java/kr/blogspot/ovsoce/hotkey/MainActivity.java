376ba985ce199612eab5ade2abdb7be8d49dd7f3
jaeho_oh <ovsoc@daum.net>
2015-10-15 10:29:06 +0900

    불필요한 코드 제거

--- app/src/main/java/kr/blogspot/ovsoce/hotkey/MainActivity.java
+++ app/src/main/java/kr/blogspot/ovsoce/hotkey/MainActivity.java
@@ -1,6 +1,5 @@
 package kr.blogspot.ovsoce.hotkey;
 
-import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,7 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
