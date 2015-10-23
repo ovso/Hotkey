48296aa5f9a6b6538bc52653510f4b48220af49e
jaeho_oh <ovsoc@daum.net>
2015-10-23 10:00:01 +0900

    Application class 생성 및 데이터베이스 초기화

---
+++ app/src/main/java/kr/blogspot/ovsoce/hotkey/application/MyApplication.java
@@ -0,0 +1,24 @@
+package kr.blogspot.ovsoce.hotkey.application;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import kr.blogspot.ovsoce.hotkey.R;
+import kr.blogspot.ovsoce.hotkey.db.DatabaseHelper;
+
+/**
+ * Created by ovso on 2015. 10. 22..
+ */
+public class MyApplication extends Application {
+    DatabaseHelper mDatabaseHelper;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mDatabaseHelper = new DatabaseHelper(getApplicationContext());
+        mDatabaseHelper.initDB(mDatabaseHelper.getWritableDatabase());
+
+    }
+    public DatabaseHelper getDatabaseHelper() {
+        return mDatabaseHelper;
+    }
+}
