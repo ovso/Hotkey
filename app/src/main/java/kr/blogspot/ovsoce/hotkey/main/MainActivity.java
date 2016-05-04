2235d95927e2d64cb8c3e799d9c0a27f201e272c
jaeho_oh <ovsoc@daum.net>
2016-05-05 06:27:58 +0900

    1. WhoFragment 제거 2. 불필요한 코드 제거

--- app/src/main/java/kr/blogspot/ovsoce/hotkey/main/MainActivity.java
+++ app/src/main/java/kr/blogspot/ovsoce/hotkey/main/MainActivity.java
@@ -1,6 +1,5 @@
 package kr.blogspot.ovsoce.hotkey.main;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -14,7 +13,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
