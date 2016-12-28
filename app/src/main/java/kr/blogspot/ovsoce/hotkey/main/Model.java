aa55d3f78c47d58a5ea6bd864e4543efa6194f71
jaeho_oh <ovsoc@daum.net>
2016-12-28 18:27:18 +0900

    Analytics 전송 구현..

--- app/src/main/java/kr/blogspot/ovsoce/hotkey/main/Model.java
+++ app/src/main/java/kr/blogspot/ovsoce/hotkey/main/Model.java
@@ -4,12 +4,33 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
+import kr.blogspot.ovsoce.hotkey.application.MyApplication;
+
 public class Model {
     protected Context mContext;
     public Model(Context context) {
         mContext = context;
+        initAnalytics();
     }
 
+    private Tracker mTracker;
+    private void initAnalytics() {
+        MyApplication application = (MyApplication)mContext.getApplicationContext();
+        mTracker = application.getDefaultTracker();
+    }
+    public void setScreenTracker(String screenName) {
+        mTracker.setScreenName(screenName);
+        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+    }
+    public void setSendTracker(String action) {
+        mTracker.send(new HitBuilders.EventBuilder()
+                .setCategory("Action")
+                .setAction(action)
+                .build());
+    }
     public String getVersionName() {
         PackageManager manager = mContext.getPackageManager();
         try {
