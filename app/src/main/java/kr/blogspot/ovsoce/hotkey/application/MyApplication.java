cc7e511a205906500ad06c2cb3b5ad9f643d54bc
ohjaeho <ovsoce@gmail.com>
2017-09-05 00:49:29 +0900

    Bugfix : Change font, Change font size

--- app/src/main/java/kr/blogspot/ovsoce/hotkey/application/MyApplication.java
+++ app/src/main/java/kr/blogspot/ovsoce/hotkey/application/MyApplication.java
@@ -4,9 +4,11 @@ import android.app.Application;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import com.facebook.stetho.Stetho;
+import kr.blogspot.ovsoce.hotkey.R;
 import kr.blogspot.ovsoce.hotkey.common.TypefaceUtil;
 import kr.blogspot.ovsoce.hotkey.db.DatabaseHelper;
 import lombok.Getter;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class MyApplication extends Application {
 
@@ -17,18 +19,30 @@ public class MyApplication extends Application {
     super.onCreate();
     mDatabaseHelper = new DatabaseHelper(getApplicationContext());
     Stetho.initializeWithDefaults(this);
-    setFonts();
     instance = this;
+    setFont();
+    setFontSize();
+  }
+
+  private void setFontSize() {
+    SharedPreferences sharedPreferences =
+        PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+    float fonts_size = Float.parseFloat(sharedPreferences.getString("fonts_size", "1.0"));
+    TypefaceUtil.fontsSize(getApplicationContext(), fonts_size);
+    sharedPreferences.edit().putString("fonts_size", String.valueOf(fonts_size)).apply();
   }
 
   /**
    * 글꼴을 설정하는 메서드
    */
-  private void setFonts() {
-    SharedPreferences sharedPreferences =
-        PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+  public static void setFont() {
+    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(instance);
     String fonts = sharedPreferences.getString("fonts", "NanumBarunGothic.ttf");
-    TypefaceUtil.overrideFont(getApplicationContext(), "SERIF", "fonts/" + fonts);
+
+    CalligraphyConfig.initDefault(new CalligraphyConfig.Builder().setDefaultFontPath(fonts)
+        .setFontAttrId(R.attr.fontPath)
+        .build());
+    sharedPreferences.edit().putString("fonts", fonts).apply();
   }
 
   public DatabaseHelper getDatabaseHelper() {
