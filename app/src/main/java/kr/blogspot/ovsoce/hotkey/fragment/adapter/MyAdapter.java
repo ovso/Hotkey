99b1377aa38040c0971444de9471d48688487b1c
jaeho <ovsoce@gmail.com>
2020-04-03 22:44:52 +0900

    Refactoring

--- app/src/main/java/kr/blogspot/ovsoce/hotkey/fragment/adapter/MyAdapter.java
+++ app/src/main/java/kr/blogspot/ovsoce/hotkey/fragment/adapter/MyAdapter.java
@@ -8,14 +8,15 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import com.jakewharton.rxbinding2.view.RxView;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import kr.blogspot.ovsoce.hotkey.App;
 import kr.blogspot.ovsoce.hotkey.R;
 import kr.blogspot.ovsoce.hotkey.fragment.vo.ContactsItem;
@@ -24,75 +25,71 @@ import kr.blogspot.ovsoce.hotkey.fragment.vo.ContactsItem;
  * RecyclerView Adapter
  */
 public class MyAdapter extends RecyclerView.Adapter {
-    private List<ContactsItem> mList;
-    private OnAdapterItemClickListener mListener;
-    private Disposable subscribe;
+  private List<ContactsItem> mList;
+  private OnAdapterItemClickListener mListener;
+  private Disposable subscribe;
 
-    public MyAdapter(List<ContactsItem> list, OnAdapterItemClickListener listener) {
-        mList = list;
-        mListener = listener;
+  public MyAdapter(List<ContactsItem> list, OnAdapterItemClickListener listener) {
+    mList = list;
+    mListener = listener;
+  }
+
+  @Override
+  public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout
+      .recyclerview_item, null);
+    return new MyViewHolder(layoutView);
+  }
+
+  @Override
+  public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    ContactsItem data = mList.get(position);
+    final MyViewHolder myViewHolder = (MyViewHolder) holder;
+
+    App app = (App) myViewHolder.blockV.getContext()
+      .getApplicationContext();
+    String colorCode = app.getDatabaseHelper().getDefaultColors()[Integer.parseInt(data
+      .getColor())];
+
+    myViewHolder.blockV.setBackgroundColor(Color.parseColor(colorCode));
+    myViewHolder.nameTv.setText(data.getName());
+    subscribe = RxView.clicks(myViewHolder.itemView).throttleFirst(2, TimeUnit
+      .SECONDS, AndroidSchedulers
+      .mainThread()).observeOn(AndroidSchedulers.mainThread()).subscribe(
+      o -> mListener
+        .onClick(myViewHolder.itemView));
+    myViewHolder.itemView.setOnLongClickListener(mListener);
+  }
+
+  public void setUpdateItem(ContactsItem item) {
+    mList.set(Integer.parseInt(item.getId()), item);
+  }
+
+  @Override
+  public int getItemCount() {
+    return mList.size();
+  }
+
+  public void onDetach() {
+    if (subscribe != null) {
+      subscribe.dispose();
     }
+  }
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout
-                .recyclerview_item, null);
-        return new MyViewHolder(layoutView);
+  public interface OnAdapterItemClickListener extends android.view.View.OnClickListener,
+    android.view.View.OnLongClickListener {
+
+  }
+
+  static class MyViewHolder extends RecyclerView.ViewHolder {
+    @BindView(R.id.tv_name)
+    TextView nameTv;
+    @BindView(R.id.v_block)
+    View blockV;
+
+    MyViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.bind(this, itemView);
     }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        ContactsItem data = mList.get(position);
-        final MyViewHolder myViewHolder = (MyViewHolder) holder;
-
-        App app = (App) myViewHolder.blockV.getContext()
-                .getApplicationContext();
-        String colorCode = app.getDatabaseHelper().getDefaultColors()[Integer.parseInt(data
-                .getColor())];
-
-        myViewHolder.blockV.setBackgroundColor(Color.parseColor(colorCode));
-        myViewHolder.nameTv.setText(data.getName());
-        subscribe = RxView.clicks(myViewHolder.itemView).throttleFirst(2, TimeUnit
-                .SECONDS, AndroidSchedulers
-                .mainThread()).observeOn(AndroidSchedulers.mainThread()).subscribe(
-            new Consumer<Object>() {
-                @Override public void accept(Object o) throws Exception {
-                    mListener
-                        .onClick(myViewHolder.itemView);
-                }
-            });
-        myViewHolder.itemView.setOnLongClickListener(mListener);
-    }
-
-    public void setUpdateItem(ContactsItem item) {
-        mList.set(Integer.parseInt(item.getId()), item);
-    }
-
-    @Override
-    public int getItemCount() {
-        return mList.size();
-    }
-
-    public void onDetach() {
-        if (subscribe != null) {
-            subscribe.dispose();
-        }
-    }
-
-    public interface OnAdapterItemClickListener extends android.view.View.OnClickListener,
-            android.view.View.OnLongClickListener {
-
-    }
-
-    static class MyViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.tv_name)
-        TextView nameTv;
-        @BindView(R.id.v_block)
-        View blockV;
-
-        MyViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-        }
-    }
+  }
 }
