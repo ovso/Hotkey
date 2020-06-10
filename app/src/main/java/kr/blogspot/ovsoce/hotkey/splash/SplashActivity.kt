dd968e809bc081dde4ba376293b468517e59c196
jaeho <ovsoce@gmail.com>
2020-06-11 07:06:39 +0900

    Update gradle, Create Splash

---
+++ app/src/main/java/kr/blogspot/ovsoce/hotkey/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package kr.blogspot.ovsoce.hotkey.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.blogspot.ovsoce.hotkey.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
